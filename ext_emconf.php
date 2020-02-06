4eed12fd92595ca271665da3e8be5f1d158b9a76
Stefan Froemken <froemken@gmail.com>
2020-02-06 13:55:24 +0100

    Update requirement of maps2 to version 7.1.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,11 +17,11 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '2.1.0',
+    'version' => '2.1.1',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-9.99.99',
-            'maps2' => '5.0.0-5.99.99'
+            'maps2' => '7.1.3-7.99.99'
         ],
         'conflicts' => [],
         'suggests' => []
