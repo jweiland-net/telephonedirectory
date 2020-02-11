502cccd6825840e038fb4b3935d8f47af5324e0a
Stefan Froemken <froemken@gmail.com>
2020-02-11 09:33:09 +0100

    Update to version 2.1.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,11 +17,11 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '2.1.1',
+    'version' => '2.1.2',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-9.99.99',
-            'maps2' => '7.1.3-7.99.99'
+            'maps2' => '8.0.0-0.0.0'
         ],
         'conflicts' => [],
         'suggests' => []
