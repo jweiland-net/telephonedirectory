9bc8bc7262e97d0404a7f0719ca8557fb4d6a02d
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-07-16 12:06:44 +0200

    [BUGFIX] Configuration for Category Model added as the old version removed from Core

---
+++ Classes/Domain/Model/Category.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/telephonedirectory.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\Telephonedirectory\Domain\Model;
+
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+/**
+ * Class which contains all setters and getters for Category
+ */
+class Category extends AbstractEntity
+{
+    protected string $title = '';
+
+    protected string $description = '';
+
+    protected Category $parent;
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getParent(): Category
+    {
+        if ($this->parent instanceof LazyLoadingProxy) {
+            $this->parent->_loadRealInstance();
+        }
+        return $this->parent;
+    }
+
+    public function setParent(Category $parent): void
+    {
+        $this->parent = $parent;
+    }
+}
---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    \JWeiland\Telephonedirectory\Domain\Model\Category::class => [
+        'tableName' => 'sys_category',
+    ],
+];
