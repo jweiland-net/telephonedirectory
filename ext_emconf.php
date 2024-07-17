c214a2cdae24a18fdd7ad309800631cd1ebf96f4
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-07-17 15:39:50 +0200

    [TASK] Fixed Version details and dependencies for extension

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,8 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-12.4.99',
-            'maps2' => '11.0.0-11.9.99'
+            'maps2' => '11.0.0-11.9.99',
+            'checkfaluploads' => '4.0.0-4.9.99'
         ],
         'conflicts' => [],
         'suggests' => [
