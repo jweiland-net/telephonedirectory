30810eba55dd4c1822948751cce364ce46e8c39a
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-08-13 19:17:14 +0200

    [TASK] Test Suite updated by removing docker-compose

--- Build/phpunit/FunctionalTestsBootstrap.php
+++ Build/phpunit/FunctionalTestsBootstrap.php
@@ -7,11 +7,9 @@
  * LICENSE file that was distributed with this source code.
  */
 
-use TYPO3\TestingFramework\Core\Testbase;
-
-call_user_func(function () {
-    $testbase = new Testbase();
+(static function () {
+    $testbase = new \TYPO3\TestingFramework\Core\Testbase();
     $testbase->defineOriginalRootPath();
     $testbase->createDirectory(ORIGINAL_ROOT . 'typo3temp/var/tests');
     $testbase->createDirectory(ORIGINAL_ROOT . 'typo3temp/var/transient');
-});
+})();
