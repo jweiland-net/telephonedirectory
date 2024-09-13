070ba28863625326ba6856ba1fbce59a59b9c742
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-09-13 21:14:56 +0200

    [TASK] Removed table definition of `tx_telephonedirectory_office_mm`

--- ext_tables.sql
+++ ext_tables.sql
@@ -94,21 +94,6 @@ CREATE TABLE tx_telephonedirectory_domain_model_language
 );
 
 #
-# Table structure for table 'tx_telephonedirectory_office_mm'
-#
-CREATE TABLE tx_telephonedirectory_office_mm
-(
-	uid_local       int(11) DEFAULT '0' NOT NULL,
-	uid_foreign     int(11) DEFAULT '0' NOT NULL,
-	tablenames      varchar(255) DEFAULT '' NOT NULL,
-	fieldname       varchar(255) DEFAULT '' NOT NULL,
-	sorting         int(11) DEFAULT '0' NOT NULL,
-	sorting_foreign int(11) DEFAULT '0' NOT NULL,
-	KEY             uid_local (uid_local),
-	KEY             uid_foreign (uid_foreign)
-);
-
-#
 # Table structure for table 'tx_telephonedirectory_domain_model_office_department_mm'
 #
 #
