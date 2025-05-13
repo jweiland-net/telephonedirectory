d9252a020a8cb478e24c17192c13e060a47d1d94
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-05-13 12:59:35 +0200

    [TASK] Fixed CGL issues

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,7 +1,14 @@
 <?php
 
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+/*
+ * This file is part of the package jweiland/telephonedirectory.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 /*
  * This file is part of the package jweiland/telephonedirectory.
